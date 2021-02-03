9ec52e8a81eab36bd4d72ad89b0a41772d5c15de
fhernan22 <fhern103@fiu.edu>
2021-02-03 16:47:04 -0500

    Cleaned up code

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,5 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from "./App";
-import Home from "./components/Home";
 
 test("renders learn react link", () => {
   render(<App />);

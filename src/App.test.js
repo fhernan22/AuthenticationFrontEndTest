80f2c4741d54ffef8b7388119762cb6df1566b9c
fhernan22 <fhern103@fiu.edu>
2021-02-03 16:46:25 -0500

    Erased testing code

---
+++ src/App.test.js
@@ -0,0 +1,7 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import Home from "./components/Home";
+
+test("renders learn react link", () => {
+  render(<App />);
+});
